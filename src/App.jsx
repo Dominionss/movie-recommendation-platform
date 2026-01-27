62f5c956f6f0820d62e73aa11094b612db4d0bd8
Dominionss <qshieldt@gmail.com>
2026-01-27 22:10:00 +0200

    I will do it!

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
 
 function App() {
-  return (
-      <>
-          <h1>Movie recommendation platform</h1>
+    return (
+        <>
+            <h1>Movie recommendation platform</h1>
 
-          <h2>Daily recommendations</h2>
+            <h2>Daily recommendations</h2>
 
-          <h2>Movies</h2>
+            <h2>Movies</h2>
 
-          <p>I will recommend movies here!</p>
+            <p>I will recommend movies here!</p>
 
-          <p>About our platform.</p>
+            <p>About our platform.</p>
 
-          <p>What's your favorite movie? </p>
+            <p>What's your favorite movie? </p>
 
-          <p>Yeeeey!</p>
+            <p>Yeeeey!</p>
 
-          <p>Leave a review to that movie.</p>
+            <p>Leave a review to that movie.</p>
 
-          <p>Let's try not to forget about commits!</p>
+            <p>Let's try not to forget about commits!</p>
 
-          <p>Commit!</p>
+            <p>Commit!</p>
 
-          <p>Here again!</p>
+            <p>Here again!</p>
 
-          <p>22 yeeep!</p>
+            <p>22 yeeep!</p>
 
-          <p>I need more coding!</p>
+            <p>I need more coding!</p>
 
-      </>
-  )
+            <p>I will do it, really!</p>
+
+        </>
+    )
 }
 
 export default App;
