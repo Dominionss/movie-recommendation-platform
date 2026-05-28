424b4789406fd7efa58f91ab14a2edb7e4394ecb
Dominionss <qshieldt@gmail.com>
2026-05-28 12:45:53 +0300

    Tailwind first use

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function Navbar() {
+    return(
+        <>
+            <nav className="bg-gray-800 p-4 text-white">Navbar</nav>
+    </>
+    );
+}
+
+export default Navbar;
